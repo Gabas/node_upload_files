9ed990d9c05a4df52eed150f507f24e48de457df
Roman Gusev <Gabas@Romans-MacBook-Pro.local>
2015-09-07 14:02:23 -0700

    Working program

---
+++ frequency.js
@@ -0,0 +1,13 @@
+var arr = ["apple", "banana", "carpet", "bin", "drill"];
+
+var max = {};
+
+for (var i = 0; i < arr.length; i++) {
+
+	for (var j = 0; i < arr.length; j++) {
+		if (arr[i].charAt(0) == arr[j].charAt(0)) {
+			
+		}
+	}
+
+}
